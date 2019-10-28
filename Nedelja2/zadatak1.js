1c8a8130d4396416ee1e2d3e44bec0543c4baddc
Jelena Andjelkovic <jelena.andjelkovic@gmail.com>
2019-10-28 21:27:13 +0100

    pon

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,22 @@
+// zadatak 1
+
+var a = 4;
+var b = 2;
+
+var zbir = a + b;
+console.log(zbir);
+
+var razlika = a - b;
+console.log(razlika);
+
+var proizvod = a * b;
+console.log(proizvod);
+
+var kolicnik = a / b;
+console.log(kolicnik);
+
+var ostatak = a % b;
+console.log(ostatak);
+
+var stepen = Math.pow(a, b);
+console.log(stepen);
---
+++ Nedelja2/zadatak2.js
@@ -0,0 +1,7 @@
+// document.getElementById("hello").innerHTML = "My First JavaScript";
+
+var welcome = 'Welcome to DOM'
+document.getElementById('hello').innerHTML = welcome;
+
+document.getElementById('new').innerHTML = `<p>Very often when you write code, you want to perform different actions for different decisions.
+You can use conditional statements in your code to do this.</p>`
